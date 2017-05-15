426a099299a60eb195a9eadb1b33908ef86c9e16
Philipeano <philipeano@gmail.com>
2017-05-15 14:25:48 +0100

    Update tests.js

--- tests/tests.js
+++ tests/tests.js
@@ -7,27 +7,27 @@ const myApp = require('../app/library.js');
 
 describe("Arithmetic Functionality", function() {
   it("should return 2 as average for 1, 2, 3", function() {
-    assert(myApp.computeAverage(1, 2, 3) == 2);
+    assert.equal(myApp.computeAverage(1, 2, 3), 2);
   })
   it("should return 5 as average for 3, 7, 5", function() {
-    assert(myApp.computeAverage(3, 7, 5) == 5);
+    assert.equal(myApp.computeAverage(3, 7, 5), 5);
   })
   it("should return 120 as factorial for 5", function() {
-    assert(myApp.computeFactorial(5) == 120);
+    assert.equal(myApp.computeFactorial(5), 120);
   })
 })
 
 describe("Temperature Conversion Functionality", function() {
   it("should return 104 for Celcius value 40", function() {
-    assert(myApp.convertTempCtoF(40) == 104);
+    assert.equal(myApp.convertTempCtoF(40), 104);
   })
   it("should return 140 for Celcius value 60", function() {
-    assert(myApp.convertTempCtoF(60) == 140);
+    assert.equal(myApp.convertTempCtoF(60), 140);
   })
   it("should return 60 for Fahrenheit value 140", function() {
-    assert(myApp.convertTempFtoC(140) == 60);
+    assert.equal(myApp.convertTempFtoC(140), 60);
   })
   it("should return 40 for Fahrenheit value 104", function() {
-    assert(myApp.convertTempFtoC(104) == 40);
+    assert.equal(myApp.convertTempFtoC(104), 40);
   })
 })
